ee3b8a01316f294e6e6113cbef9e91a466eb848c
AdminAkai <akatoirotech@gmail.com>
2024-07-18 17:22:10 -0400

    added Guess struct and impl to game

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,24 @@ use std::io;
 use std::cmp::Ordering;
 use rand::Rng;
 
+pub struct Guess {
+    value: i32
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {value}");
+        }
+
+        Guess { value }
+    }
+
+    pub fn value(&self) -> i32 {
+        self.value
+    }
+}
+
 fn main() {
     println!("Guess the number!");
 
@@ -15,14 +33,16 @@ fn main() {
         io::stdin().read_line(&mut user_input)
             .expect("Failed to read line.");
     
-        let user_input: u32 = match user_input.trim().parse() {
+        let user_input: i32 = match user_input.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
         };
     
-        println!("You guessed: {}", user_input);
+        let guess: Guess = Guess::new(user_input);
+
+        println!("You guessed: {}", guess.value());
     
-        match user_input.cmp(&secret_number) {
+        match guess.value().cmp(&secret_number) {
             Ordering::Less => println!("Higher!"),
             Ordering::Greater => println!("Lower!"),
             Ordering::Equal => {
